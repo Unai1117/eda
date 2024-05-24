b07dec1afeb1f0b7625c5f5e93b9fd42fc7b90c9
Unai López <unailopez1117@gmail.com>
2024-05-24 09:40:47 +0200

    .

---
+++ libraries/dataStructures/graphs/Edge.java
@@ -0,0 +1,72 @@
+package libraries.dataStructures.graphs;
+
+/**
+ * Edge Class: represents an edge in a graph.<br>
+ *
+ * @version november 2021
+ */
+
+public class Edge {
+
+    // AN Edge HAS A source vertex AND A target vertex:
+    /* TO BE COMPLETED */
+    // AN Edge HAS A weight:
+    /* TO BE COMPLETED */
+    protected int source, target;
+    protected double weight;
+
+    /**
+     * Creates an edge (a, b) with weight w.
+     *
+     * @param a Source vertex
+     * @param b Target vertex
+     * @param w Weight
+     */
+    public Edge(int a, int b, double w) {
+        /* TO BE COMPLETED */
+        this.source = a;
+        this.target = b;
+        this.weight = w;
+    }
+
+    /**
+     * Returns the source vertex of an edge.
+     *
+     * @return int source vertex
+     */
+    public int getSource() {
+        // CHANGE ME
+        return this.source;
+    }
+
+    /**
+     * Returns the target vertex of an edge.
+     *
+     * @return int target vertex
+     */
+    public int getTarget() {
+        // CHANGE ME
+        return this.target;
+    }
+
+    /**
+     * Reterns an edge's weight
+     *
+     * @return double Weight of the edge
+     */
+    public double getWeight() {
+        // CHANGE ME
+        return this.weight;
+    }
+
+    /**
+     * Returns a String that represents an edge
+     * in the format (source, target, weight)
+     *
+     * @return String that represents the edge
+     */
+    public String toString() {
+        // CHANGE ME
+        return "(" + this.source + ", " + this.target + ", " + this.weight + ")";
+    }
+}
